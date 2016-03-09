e20b5932eb5666be76d87f12c292089fe2954c79
Bcaskey <bcaskeyu@charter.net>
2016-03-09 15:10:05 -0600

    updating for jquery lesson

--- JQuery/script.js
+++ JQuery/script.js
@@ -8,74 +8,62 @@ $(btn1).click(function(){
     })
 var btn1_text = document.createTextNode('Click Me');
 btn1.appendChild(btn1_text);
-//document.body.appendChild(btn1);
 $( task1 ).append( btn1 );
-});
-
 // --------- TASK 2 ---------------
 
-// var checkText = function () {
-//     var text = document.getElementById('#checkMe').value;
-//     alert(text);
-// }
+$('.checkText').click(function() {
+    var text = $( "#checkMe" ).val();
+    alert(text);
+});
+
 
 // // --------- TASK 3 ---------------
 
-// chgDiv3 = function () {
-//     var myDiv3 = document.getElementById('task3_div');
-//     myDiv3.addEventListener('mouseover', chgColor1);
-//     myDiv3.addEventListener('mouseout', chgColor2);
 
-//     function chgColor1(e) {
-//         e.target.style.backgroundColor = 'red';
-//     }
-//     function chgColor2(e) {
-//         e.target.style.backgroundColor = 'green';
-//     }
-// }
+$(".task3_divClass").on('mouseover', function(){
+        this.style.backgroundColor = 'green';
+        }).on('mouseout', function(){
+        this.style.backgroundColor = 'red';
+    })
 
+    
 // // --------- TASK 4 ---------------
 
-// chgDiv4 = function () {
-//     var myDiv4 = document.getElementById('task4_div');
-//     myDiv4.addEventListener('click', getRandomColor);
+$('#task4_div').click(function(e){
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            e.target.style.color = color;
+})
 
-//         function getRandomColor(e) {
-//         var letters = '0123456789ABCDEF'.split('');
-//         var color = '#';
-//         for (var i = 0; i < 6; i++) {
-//                 color += letters[Math.floor(Math.random() * 16)];
-//             }
-//             e.target.style.color = color;
-//         }
-//     }
-    
 // // --------- TASK 5 ---------------
 
-// task5 = function () {
-//     var myDiv5 = document.getElementById('task5_div');
-//     myDiv5.addEventListener('click', appendText5);
-    
-//     function appendText5 () {
-//         var getDiv = document.getElementById("task5_div");
-//         var newSpan = document.createElement("span");
-//         newSpan.innerHTML = "<p>Brian Caskey</p>";
-//         getDiv.appendChild(newSpan);
-//     }
-    
-// }
+$('#button5').click(function(){
+    var getDiv = document.getElementById("task5_div");
+    var newSpan = document.createElement("span");
+    newSpan.innerHTML = "<p>Brian Caskey</p>";
+    getDiv.appendChild(newSpan);
+})
 
 // // --------- TASK 6 ---------------
 
-
-// task6 = function () {
-//     myFriends = ['Mickey', 'Pluto', 'Donald', 'Goofy', 'Grumpy', 'Doc', 'Woody', 'Buzz', 'Rex', 'Mater'];
-//     var i;
-//     for (i = 0; i < myFriends.length; i++) {
-//         newFriend = myFriends[i];
-//         var myDiv6 = document.getElementById('task6_list');
-//         var newListItem = document.createElement("li");
-//         newListItem.innerHTML = myFriends[i];
-//         myDiv6.appendChild(newListItem);        
-//     }
-// }   
+task6 = function () {
+    var i;
+    for (i = 0; i < myFriends.length; i++) {
+        newFriend = myFriends[i];
+        var myDiv6 = document.getElementById('task6_list');
+        var newListItem = document.createElement("li");
+        newListItem.innerHTML = myFriends[i];
+        myDiv6.appendChild(newListItem);        
+    }
+}  
+$('#button6').click(function(){
+    myFriends = ['Mickey', 'Pluto', 'Donald', 'Goofy', 'Grumpy', 'Doc', 'Woody', 'Buzz', 'Rex', 'Mater'];
+    for (i = 0; i < myFriends.length; i++) {
+     var myFriendsLi = $('<li></li>').text(myFriends[i]);
+     $('#task6_list').append(myFriendsLi);   
+    }
+    })
+});
